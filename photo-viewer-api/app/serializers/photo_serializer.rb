a0145a4ff924908d38a996567f196cbfcd5645d9
Gulnoza Muminova <gmcharmy@gmail.com>
2020-05-04 04:21:23 -0400

    update

--- photo-viewer-api/app/serializers/photo_serializer.rb
+++ photo-viewer-api/app/serializers/photo_serializer.rb
@@ -1,3 +1,4 @@
 class PhotoSerializer < ActiveModel::Serializer
+  
   attributes :id, :url, :dimensions 
 end
--- photo-viewer-app/src/App.js
+++ photo-viewer-app/src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Photos from './Components/Photos'
+class App extends React.Component {
 
-function App() {
+  state={
+    photos: null,
+    user: null,
+    dimentions: null
+  }
+   filterHendler = (value) => {
+     console.log(value)
+     fetch(`https://photo-viewer-apii.herokuapp.com/photos?filter=${value}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        this.setState({
+          photos: res
+        })
+      }
+      )
+  } 
+componentDidMount(){
+  fetch('https://photo-viewer-apii.herokuapp.com/photos')
+    .then(res => res.json())
+    .then(res => {
+       
+        this.setState({ photos: res })
+      }
+    )
+  fetch('http://localhost:3000/photos/dimensions')
+    .then(res => res.json())
+    .then(res => {
+      this.setState({ dimentions: res.data })
+    }
+    )
+  
+}
+  render(){
+    
   return (
-    <div>
-      Hello world!
-    </div>
+    <Photos dimensions={this.state.dimentions} photos={this.state.photos} filterHendler={this.filterHendler}/>
+     
   );
+}
+  
 }
 
 export default App;
